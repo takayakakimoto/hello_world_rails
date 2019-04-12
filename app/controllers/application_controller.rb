fedc843a174549fdc4db8a76c0b1cd7cfc758186
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-04-12 22:56:13 +0900

    修正版

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def current_user
-    @current_user = User.first
+    @current_user ||= User.first
   end
 end
