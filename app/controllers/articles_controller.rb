057d48aeb1822578c4ee2df7491f7d6a7d317c52
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-04-13 20:45:12 +0900

    status #2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.published.all.includes(:user)
+    @articles = Article.published
   end
 
   # GET /articles/1
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_04_13_105443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.integer "status"
+    t.integer "status", default: 0, null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
